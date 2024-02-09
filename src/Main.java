99bebe3055f3a3bacbf3b3e7d172f81f75989db1
Guzelshi <159537803+Guzelshi@users.noreply.github.com>
2024-02-09 16:21:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое целое число (a):");
+        int a = scanner.nextInt();
+
+        System.out.println("Введите второе целое число (b):");
+        int b = scanner.nextInt();
+
+        // Сравнение чисел
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        // Операции с числами
+        System.out.println("Сумма a и b: " + (a + b));
+        System.out.println("Вычитание a - b: " + (a - b));
+
+        if (b != 0) {
+            System.out.println("Деление a на b: " + ((double) a / b));
+        } else {
+            System.out.println("Деление на 0 невозможно");
+        }
+
+        System.out.println("Умножение a на b: " + (a * b));
     }
 }
